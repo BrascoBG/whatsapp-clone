c134a3cdc02a8675e9394c8dd0035813356d0c8f
Ivo Krastev <ivo.krustev@yahoo.com>
2020-11-12 13:57:23 +0200

    working on sidebar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAq08hMhgjwL58G9u5KQ3hNDl22QkY2GOk",
+  authDomain: "whatsapp-clone-42ba1.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-42ba1.firebaseio.com",
+  projectId: "whatsapp-clone-42ba1",
+  storageBucket: "whatsapp-clone-42ba1.appspot.com",
+  messagingSenderId: "683606661299",
+  appId: "1:683606661299:web:5ba053143d8833fce5f8c6",
+  measurementId: "G-CZJ8BZMESD",
+};

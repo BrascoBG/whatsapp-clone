0e769e3ee4abfc37a539dbb6e12ec9d4a961a869
Ivo Krastev <ivo.krustev@yahoo.com>
2020-11-15 08:27:44 +0200

    Login and state managment created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Chat from "./components/Chat/Chat";
+import Login from "./components/Login/Login";
 import Sidebar from "./components/Sidebar/Sidebar";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Sidebar />
-              <Chat />
-            </Route>
-            <Route path="/">
-              <Sidebar />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Sidebar />
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Sidebar />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
